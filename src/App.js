062c5d0a265ebe31cb876703212c321f03975573
abethel912 <abethel180@gmail.com>
2022-12-18 20:15:25 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+import data from './WeatherData.js'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My WeatherIcons App</h1>
+    </div>
+  )
+}
