38c1ee1ffab076dfcb0fe7f5533d242e47d26ff1
abethel912 <abethel180@gmail.com>
2022-12-19 20:26:08 -0500

    WeatherForecast component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './styles.css'
 import data from './WeatherData.js'
+import WeatherForecast from './WeatherForecast'
 
-export default function App() {
+function App() {
   return (
     <div className="App">
+      <br></br>
       <h1>My WeatherIcons App</h1>
+      {data.map(item => <WeatherForecast data={ item } />)}
     </div>
   )
 }
+
+export default App;
---
+++ src/WeatherForecast.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles.css'
+
+
+function WeatherForecast(props) {
+  const item = props.data
+  console.log('weather data', props)
+
+  return (
+    <>
+      <div className="weather-container">
+        <div>
+          <img src={item.img} alt={item.title} />
+          <p>Conditions:{item.conditions}</p>
+          <p>Time:{item.time}</p>
+        </div>
+      </div>
+    </>
+  )
+};
+      
+
+
+export default WeatherForecast;
+        
+        // <img className="Image" src={props.data.img} alt="" />
